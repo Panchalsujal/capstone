a8d32c64eb8ace114c91b213963ecb4cda821e6c
Sujal Panchal <Sujalpanchal249@gmail.com>
2026-07-01 10:31:23 +0530

    feat(agent): initialize agent server with Express and basic routes
    - Created package.json for project dependencies and scripts.
    - Added server.js to start the Express server on port 3000.
    - Implemented app.js with a basic "Hello" endpoint and a route to list files in the working directory.

---
+++ sandbox/agent/src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import morgan from "morgan";
+import fs from "fs";
+const app = express();
+const WORKING_DIR = "/workspace";
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello",
+    status: "Success",
+  });
+});
+
+app.get("list-files", async (req, res) => {
+  const elements = await fs.promises.readdir(WORKING_DIR);
+
+  res.status(200).json({
+    message: "Elements in working Directory",
+    elements,
+  });
+});
+
+export default app;
