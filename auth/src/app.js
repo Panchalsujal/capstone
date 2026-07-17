5cc930757d79f00b8752ac8853d01f0e4c5460cd
Sujal Panchal <Sujalpanchal249@gmail.com>
2026-07-18 01:43:31 +0530

    feat(auth): set up server and initialize app with basic routing

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,3 +1,8 @@
-import "dotenv/config"
-import express from 'express'
-import morgam from 'morgan'
+import "dotenv/config";
+import express from "express";
+import morgam from "morgan";
+
+const app = express();
+app.use(express.json());
+
+export default app;
